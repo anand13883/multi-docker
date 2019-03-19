3f9bc2104acbed5d202fd23e6dc4eee1f8a78c6b
Anand Poddar <Anand.Poddar@unico.com.au>
2019-03-20 09:26:28 +1100

    working service

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,17 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <p>
+            Edit <code>src/App.js</code> and save to reload. I was changed on the feature branch!
+			</p>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
